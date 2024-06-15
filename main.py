2710c435ede6b59fcb4c7aea74d298e444c5e793
Madhav V Rajeev <75824625+GodOfCoding1@users.noreply.github.com>
2024-06-15 12:16:45 +0530

    added init

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
 from pose import PoseChecker
+from mouth import MouthChecker
+import cv2
+from time import sleep
+
+def is_pose_correct(image_frame,mouth:MouthChecker,pose:PoseChecker):
+    try:
+        return True if mouth.is_pose_correct(image_frame) and pose.is_pose_correct(image_frame) else False
+    except:
+        raise ValueError("Some error occured")
+
+def init(mouth:MouthChecker,pose:PoseChecker):
+    input("press enter to capture default ratios")
+    cap = cv2.VideoCapture(0) # 0 for webcam
+    while cap.isOpened():
+        _, frame = cap.read()
+        is_mouth_ok = mouth.get_default_ratio(frame)
+        is_pose_ok = pose.get_default_ratio(frame)
+        if is_mouth_ok and is_pose_ok:
+            return
+        cap.release()
+        cv2.destroyAllWindows()
+        print("unable to detect mouth or pose or both")
+        init()
+ 
+def main():
+    mouth=MouthChecker()
+    pose=PoseChecker()
+    init(mouth,pose)
+
+
+
